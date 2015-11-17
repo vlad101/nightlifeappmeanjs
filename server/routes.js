239890f41c6589376649d85f0b1e78aedbc737a1
vlad101 <vladimir.efros@gmail.com>
2015-11-17 19:04:18 +0000

    added boileplate for twitter auth

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/auth/twitter', require('./api/auth'));
   app.use('/api/yelp', require('./api/yelp'));
   app.use('/api/things', require('./api/thing'));
   
