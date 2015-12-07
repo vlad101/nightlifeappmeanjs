ca939ea9b98d8c80e431ddf24ee713fa7bbbb8e4
vlad101 <V_efros85@hotmail.com>
2015-12-06 23:06:48 -0500

    updated npm with twitter passport and twitter callback

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'UEFRvfpvDC5IqaJriRt6yVVsp',
         'consumerSecret'     : 'LTD4Zmor7ziVFLG9GLgsRhk4uLe79qcgwSw0wGTVf3FUpUsFiZ',
-        'callbackURL'        : 'http://localhost:9000/auth/twitter/callback'
+        'callbackURL'        : 'http://morning-falls-2061.heroku.com/auth/twitter/callback'
     }
     
 };
